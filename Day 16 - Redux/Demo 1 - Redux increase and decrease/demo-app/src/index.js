9a385fda42e60c3d670fb406d31222e4420ea561
CtrShiftN <karuyanakaido24@gmail.com>
2022-02-16 22:37:37 +0700

    redux intro Signed-off-by: CtrShiftN <karuyanakaido24@gmail.com>

---
+++ Day 16 - Redux/Demo 1 - Redux increase and decrease/demo-app/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducer from './components/reducer';
+import Increasement from './components/Increasement';
+
+let store = createStore(reducer);
+ReactDOM.render(
+  <Provider store={store}>
+    <Increasement />
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
